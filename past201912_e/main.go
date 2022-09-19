82fdc8170ffef9a27cd38c1cb54f4bfcd1b03a16
basyura <basyura@gmail.com>
2022-09-19 22:46:59 +0900

    past201912_e : refactored

--- past201912_e/main.go
+++ past201912_e/main.go
@@ -35,32 +35,39 @@ func main() {
 	}
 }
 
-type Relation struct {
+type Relations map[int]bool
+
+type RelationStore struct {
 	count int
-	dic   map[int]map[int]bool
+	store map[int]Relations
 }
 
-func (self *Relation) Follow(from, to int) {
-	self.dic[from][to] = true
+func (self *RelationStore) GetRelations(uid int) Relations {
+	return self.store[uid]
 }
 
-func (self *Relation) FollowBack(from int) {
-	for k, r := range self.dic {
+func (self *RelationStore) Follow(from, to int) {
+	relation := self.store[from]
+	relation[to] = true
+}
+
+func (self *RelationStore) FollowBack(from int) {
+	for k, r := range self.store {
 		if v, ok := r[from]; ok && v {
-			self.dic[from][k] = true
+			self.store[from][k] = true
 		}
 	}
 }
 
 // フォローフォロー: その時点でユーザ a がフォローしている各ユーザ x に対し、ユーザ a が次を行う
 // ユーザ x がフォローしているすべてのユーザ (ユーザ a 自身を除く) をフォローする
-func (self *Relation) FollowFollow(from int) {
+func (self *RelationStore) FollowFollow(from int) {
 	// from's follow
-	dic := self.dic[from]
+	relations := self.GetRelations(from)
 	targets := []int{}
-	for uid := range dic {
+	for uid := range relations {
 		// follow's follow
-		for to := range self.dic[uid] {
+		for to := range self.GetRelations(uid) {
 			if to != from {
 				targets = append(targets, to)
 			}
@@ -72,8 +79,8 @@ func (self *Relation) FollowFollow(from int) {
 	}
 }
 
-func (self *Relation) Status(from int) string {
-	follows := self.dic[from]
+func (self *RelationStore) Status(from int) string {
+	follows := self.GetRelations(from)
 	s := ""
 	i := 1
 	for i <= self.count {
@@ -88,16 +95,16 @@ func (self *Relation) Status(from int) string {
 	return s
 }
 
-func newMap(n int) *Relation {
-	dic := map[int]map[int]bool{}
+func newMap(n int) *RelationStore {
+	store := map[int]Relations{}
 	i := 1
 	for i <= n {
-		dic[i] = map[int]bool{}
+		store[i] = Relations{}
 		i++
 	}
-	relation := &Relation{
+	relation := &RelationStore{
 		count: n,
-		dic:   dic,
+		store: store,
 	}
 	return relation
 }
